a6f62927fae53cfc3cb34a78c43825e46bebcfb3
Mario Josue del Toro Morales <mario_chomy@outlook.com>
2017-11-08 15:51:42 -0600

    Vector RAM

--- Procesos Windows/src/procesos/windows/ProcesosWindows.java
+++ Procesos Windows/src/procesos/windows/ProcesosWindows.java
@@ -6,11 +6,19 @@ import javax.swing.JOptionPane;
  * @author Mario Josue del Toro Morales
  */
 public class ProcesosWindows {
+    
+    int RAM[];
 
     public static void main(String[] args) {
         int opcion;
         
         opcion = JOptionPane.showOptionDialog(null, "SELECCIONE OPCIÓN", "MENU PRINCIPAL", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, new Object[]{"INICIAR PROCESO", "SALIR"}, null);
+        
+        if (opcion == 0) {
+            
+        } else {
+            
+        }
     }
     
 }
