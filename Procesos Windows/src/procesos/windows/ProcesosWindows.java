3a0dda845d4639ba2b9bad565fcdda8809b60137
Mario Josue del Toro Morales <mario_chomy@outlook.com>
2017-11-09 20:41:37 -0600

    Más cosas
    Aproximadamente al 40% del final

--- Procesos Windows/src/procesos/windows/ProcesosWindows.java
+++ Procesos Windows/src/procesos/windows/ProcesosWindows.java
@@ -1,6 +1,6 @@
-
 package procesos.windows;
 
+import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.JOptionPane;
 
@@ -18,7 +18,7 @@ public class ProcesosWindows {
         opcion = JOptionPane.showOptionDialog(null, "SELECCIONE OPCIÓN", "MENU PRINCIPAL", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, new Object[]{"INICIAR PROCESO", "SALIR"}, null);
 
         if (opcion == 0) {
-            
+
             //Tamaño de memoria de la RAM
             do {
                 salir = false;
@@ -32,11 +32,11 @@ public class ProcesosWindows {
                     } while (tamaño <= 0);
 
                 } catch (Exception e) {
-                    JOptionPane.showMessageDialog(null, "Ha ingresado algún valor que generó un conflicto \n" + e.getMessage() + "\n Favor de ingresar un valor correcto", "ERROR", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Ha ingresado algún valor que generó un conflicto \n" + e.getMessage() + "\n Favor de ingresar un valor correcto", "ERROR GENERAL", JOptionPane.ERROR_MESSAGE);
                     salir = true;
                 }
             } while (salir);
-            
+
             //Porcentaje de utilización de la RAM
             do {
                 salir = false;
@@ -50,13 +50,13 @@ public class ProcesosWindows {
                     } while (ocupado < 0 || ocupado > 100);
 
                 } catch (Exception e) {
-                    JOptionPane.showMessageDialog(null, "Ha ingresado algún valor que generó un conflicto \n" + e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Ha ingresado algún valor que generó un conflicto \n" + e.getMessage() + "\n Favor de ingresar un valor correcto", "ERROR GENERAL", JOptionPane.ERROR_MESSAGE);
                     salir = true;
                 }
             } while (salir);
 
             String RAM[] = new String[tamaño];
-            
+
             //Método para llenar espacios de forma aleatoria de la RAM
             RAM = OcupadosRAM(RAM, ocupado, tamaño);
 
@@ -64,45 +64,161 @@ public class ProcesosWindows {
             do {
                 salir = false;
                 try {
-                    
+
                     do {
                         procesos = Byte.parseByte(JOptionPane.showInputDialog(null, "¿Cuántos Procesos va a realizar?", "Numero de procesos", JOptionPane.INFORMATION_MESSAGE));
-                    
+
                         if (procesos < 1) {
-                            JOptionPane.showMessageDialog(null, "IngresÓ un número de procesos erróneo", "Procesos", JOptionPane.ERROR_MESSAGE);
+                            JOptionPane.showMessageDialog(null, "Ingresó un número de procesos erróneo", "Procesos", JOptionPane.ERROR_MESSAGE);
                         }
                     } while (procesos < 1);
-                    
+
                 } catch (Exception e) {
-                    JOptionPane.showMessageDialog(null, "Ha ingresado algún valor que generó un conflicto \n" + e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Ha ingresado algún valor que generó un conflicto \n" + e.getMessage() + "\n Favor de ingresar un valor correcto", "ERROR GENERAL", JOptionPane.ERROR_MESSAGE);
                     salir = true;
                 }
             } while (salir);
-            
-            String nombre;
-            
+
+            byte[] proc = new byte[procesos];
+
             //Número de Segemetos que va a contener cada uno de los Procesos
             do {
-                salir = false;                
-                try{
-                    
-                    for (int i = 0; i < procesos; i++) {
-                        segmentos = Byte.parseByte(JOptionPane.showInputDialog(null, "Número de segmentos que tiene el proceo " + i+1, "Segementos proceso " + i + 1, JOptionPane.QUESTION_MESSAGE));
-                        
-                       nombre = "TMS_P" + i+1;
-                        
-                        byte[][] nombre = new byte[segmentos][4];
-                    }
-                    
-                }catch (Exception e){
-                    JOptionPane.showMessageDialog(null, "Ha ingresado algún valor que generó un conflicto \n" + e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+                salir = false;
+                try {
+                    int i = 0;
+
+                    do {
+                        do {
+                            segmentos = Byte.parseByte(JOptionPane.showInputDialog(null, "Número de segmentos que tiene el proceso " + (i + 1), "Segementos proceso " + (i + 1), JOptionPane.QUESTION_MESSAGE));
+
+                            if (segmentos <= 0) {
+                                JOptionPane.showMessageDialog(null, "Ingresó un número de segmentos erróneo", "Error Segmentos", JOptionPane.ERROR_MESSAGE);
+                            }
+                        } while (segmentos <= 0);
+
+                        proc[i] = segmentos;
+
+                        i++;
+                    } while (i < procesos);
+
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Ha ingresado algún valor que generó un conflicto \n" + e.getMessage() + "\n Favor de ingresar un valor correcto", "ERROR GENERAL", JOptionPane.ERROR_MESSAGE);
                     salir = true;
                 }
-                
             } while (salir);
-            
 
-            desplegadoRAM(RAM, tamaño);
+            int h = 0;
+
+            for (int i = 0; i < proc.length; i++) {
+                h += proc[i];
+            }
+
+            int[][] TMS = new int[h][4];
+
+            String[] tipo = new String[h];
+            Byte cuantos[] = new Byte[h];
+
+            altaSegmentos(proc, h, tipo, cuantos);
+
+            //TMS = llenadoTMS(TMS, h);
+            //desplegadoRAM(RAM, tamaño);
+            //desplegadoTMS(TMS, h, proc);
+        }
+    }
+
+    //Método para dar de alta los datos de cada segmento
+    public static void altaSegmentos(byte[] proc, int h, String[] tipo, Byte[] cuantos) {
+        int k, o = 0;
+        boolean bueno;
+        String cadenota = "";
+        byte numero = 0;
+
+        for (int i = 0; i < proc.length; i++) {
+            k = 0;
+            cadenota += "\nEl Proceso " + (i + 1) + " tiene " + proc[i] + " Segmentos.";
+
+            do {
+                tipo[o] = (String) JOptionPane.showInputDialog(null, "¿Qué va a contener el Segmento " + (k + 1) + " del Proceso " + (i + 1), "ALTA DE SEGMENTOS", JOptionPane.QUESTION_MESSAGE, null, new Object[]{"PÁGINA", "VARIABLE"}, "SELECCIONE UNA OPCIÓN");
+
+                do {
+                    bueno = false;
+
+                    try {
+                        if (tipo[o].equalsIgnoreCase("PÁGINA")) {
+                            do {
+                                numero = Byte.parseByte(JOptionPane.showInputDialog(null, "Número de Páginas que contendrá el Segmento " + (k + 1) + " del Proceso " + (i + 1), "Total", JOptionPane.QUESTION_MESSAGE));
+
+                                if (numero <= 0) {
+                                    JOptionPane.showMessageDialog(null, "Ingresó un número de procesos erróneo", "Procesos", JOptionPane.ERROR_MESSAGE);
+                                }
+                            } while (numero <= 0);
+                        } else {
+                            do {
+                                numero = Byte.parseByte(JOptionPane.showInputDialog(null, "Número de Variables que contendrá el Segmento " + (k + 1) + " del Proceso " + (i + 1), "Total", JOptionPane.QUESTION_MESSAGE));
+
+                                if (numero <= 0) {
+                                    JOptionPane.showMessageDialog(null, "Ingresó un número de procesos erróneo", "Procesos", JOptionPane.ERROR_MESSAGE);
+                                }
+                            } while (numero <= 0);
+                        }
+
+                    } catch (Exception e) {
+                        JOptionPane.showMessageDialog(null, "Ha ingresado algún valor que generó un conflicto \n" + e.getMessage() + "\n Favor de ingresar un valor correcto", "ERROR GENERAL", JOptionPane.ERROR_MESSAGE);
+                        bueno = true;
+                    }
+                } while (bueno);
+
+                cuantos[i] = numero;
+
+                if (tipo[o].equalsIgnoreCase("PÁGINA")) {
+                    cadenota += "\n --> El Segmento " + (k + 1) + " tiene " + cuantos[i] + " páginas";
+                } else {
+                    cadenota += "\n --> El Segmento " + (k + 1) + " contiene " + cuantos[i] + " variables de 1 Kb c/u";
+                }
+
+                k++;
+                o++;
+
+            } while (k < proc[i]);
+            cadenota += "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, cadenota, "INFORMACIÓN", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    //Método para llenar la Tabla de Mapa de Segmentos
+    public static int[][] llenadoTMS(int[][] TMS, int h) {
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < 4; j++) {
+                TMS[i][j] = (i + j);
+            }
+        }
+
+        return TMS;
+    }
+
+    //Método para desplegar TMS
+    public static void desplegadoTMS(int[][] TMS, int h, byte proc[]) {
+        int k = 0, sum;
+
+        sum = proc[0];
+
+        for (int i = 0; i < h; i++) {
+            if (i == 0) {
+                System.out.println("\nTMS P" + (i + 1));
+            } else {
+                if (i == sum) {
+                    System.out.println("\nTMS P" + (k + 2));
+
+                    k++;
+                    sum += proc[k];
+                }
+            }
+            for (int j = 0; j < 4; j++) {
+                System.out.print("  " + TMS[i][j]);
+            }
+            System.out.println();
         }
     }
 
@@ -147,7 +263,7 @@ public class ProcesosWindows {
 
         return RAM;
     }
-    
+
     //Método para desplegar toda la RAM con sus respectivas asignaciones
     public static void desplegadoRAM(String[] RAM, int tamaño) {
         int i = 0;
